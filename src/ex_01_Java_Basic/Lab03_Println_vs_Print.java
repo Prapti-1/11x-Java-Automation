d83e535bb6967294b3582ebe767713b412cb6749
Prapti <prapticoco@gmail.com>
2025-03-11 01:29:55 +0530

    I am pushing the test code

---
+++ src/ex_01_Java_Basic/Lab03_Println_vs_Print.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab03_Println_vs_Print {
+    public static void main(String[] args) {
+        System.out.println("Hello Neko"); //print with new line
+        System.out.println("Hello Neko2"); //print with new line
+
+        System.out.print("Hello Neko"); //print with same line
+        System.out.print("Hello Neko"); //print with same line
+    }
+}
