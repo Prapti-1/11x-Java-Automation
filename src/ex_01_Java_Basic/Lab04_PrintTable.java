77901d3bc248bb5c2e32fe72e7dd05e4cc85a720
Prapti <prapticoco@gmail.com>
2025-03-11 23:36:17 +0530

    I am pushing the test code

---
+++ src/ex_01_Java_Basic/Lab04_PrintTable.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+public class Lab04_PrintTable {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("Multiplication of %d * 1 = %d%n",a , a*1);
+        System.out.printf("Multiplication of %d * 2 = %d%n",a , a*2);
+        System.out.printf("Multiplication of %d * 3 = %d%n",a , a*3);
+        System.out.printf("Multiplication of %d * 4 = %d%n",a , a*4);
+        System.out.printf("Multiplication of %d * 5 = %d%n",a , a*5);
+        System.out.printf("Multiplication of %d * 6 = %d%n",a , a*6);
+        System.out.printf("Multiplication of %d * 7 = %d%n",a , a*7);
+        System.out.printf("Multiplication of %d * 8 = %d%n",a , a*8);
+        System.out.printf("Multiplication of %d * 9 = %d%n",a , a*9);
+        System.out.printf("Multiplication of %d * 10 = %d%n",a , a*10);
+
+    }
+}
